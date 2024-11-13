ff0722ae4eac0b1239370f07d14b6e7c6e14c897
Pratik Nadgouda <nadgoudapratik@gmail.com>
2024-11-14 00:46:26 +0530

    create socials component listing the social media icons

---
+++ src/components/Socials.tsx
@@ -0,0 +1,24 @@
+import { uniqueId } from "lodash";
+import Icon from "./Icon";
+import { socialMediaLinks } from "../static/SocialMediaData";
+
+export default function Socials() {
+  return (
+    <>
+      <ul className="flex flex-col items-center m-0 p-0 after:content-[''] after:h-20 after:block after:my-0 after:mx-auto after:w-px after:bg-slate-200 ">
+        {socialMediaLinks.map((social) => (
+          <li
+            key={uniqueId("social-media-item")}
+            className="last-of-type:mb-5 hover:-translate-y-0.5 hover:text-teal-200">
+            <a href={social.url} className="p-2.5">
+              <Icon
+                icon={social.label}
+                props={{ width: "size-5", height: "size-5" }}
+              />
+            </a>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ src/static/SocialMediaData.ts
@@ -0,0 +1,17 @@
+type SocialMediaDataType = {
+  label: IconTypes;
+  url: string;
+};
+
+export const socialMediaLinks: SocialMediaDataType[] = [
+  {
+    label: "github",
+    url: "https://github.com/carbonautics",
+  },
+  {
+    label: "linkedin",
+    url: "https://linkedin.com/in/pratiknadgouda",
+  },
+];
+
+export const email: string = "nadgoudapratik@gmail.com";
